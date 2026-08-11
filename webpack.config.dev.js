0856e3e913d3269d134dbb67db0ae2877744c88d
Mathenge001 <156816578+Mathenge001@users.noreply.github.com>
2026-08-11 11:23:22 +0300

    Update webpack.config.dev.js

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,13 +1,33 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'inline-source-map',
   devServer: {
-    liveReload: true,
-    hot: true,
-    open: true,
-    static: ['./'],
+    static: './dist',
+    port: 8080,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      inject: false,
+      supabaseUrl: process.env.SUPABASE_URL || '',
+      supabaseKey: process.env.SUPABASE_PUBLISHABLE_KEY || '',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'Horizontal ID.jpg', to: 'Horizontal ID.jpg' },
+        { from: 'Horizontal ID Updated Back with QR.jpg', to: 'Horizontal ID Updated Back with QR.jpg' },
+        { from: 'Horizontal ID Back with QR.jpg', to: 'Horizontal ID Back with QR.jpg' },
+        { from: 'icon.svg', to: 'icon.svg' },
+        { from: 'favicon.ico', to: 'favicon.ico' },
+        { from: 'robots.txt', to: 'robots.txt' },
+        { from: 'icon.png', to: 'icon.png' },
+        { from: '404.html', to: '404.html' },
+        { from: 'site.webmanifest', to: 'site.webmanifest' },
+      ],
+    }),
+  ],
 });
