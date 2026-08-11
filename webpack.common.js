9dbf7eae57d69d598d3463f2f60882524767e3d4
Mathenge001 <156816578+Mathenge001@users.noreply.github.com>
2026-08-11 11:22:53 +0300

    Remove entry point from webpack configuration
    Removed entry point configuration for app.js.

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  entry: {
-    app: './js/app.js',
-  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    filename: './js/app.js',
   },
 };
